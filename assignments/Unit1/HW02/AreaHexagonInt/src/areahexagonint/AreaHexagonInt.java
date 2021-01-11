f0834a2887f68411e5c70ef8e0234688b941a979
Leonela-Comina <leonelacomina@gmail.com>
2021-01-11 14:23:09 -0500

    WS04LogicalOperations

--- assignments/Unit1/HW02/AreaHexagonInt/src/areahexagonint/AreaHexagonInt.java
+++ assignments/Unit1/HW02/AreaHexagonInt/src/areahexagonint/AreaHexagonInt.java
@@ -16,8 +16,8 @@ public class AreaHexagonInt {
     apothem = (int) (side/1.15);
     area = (int) ((3*side)*apothem);
     
-    System.out.println("the area of the hexagon which side is equal to " + side + 
-            "u" + ", and, its apothem is equal to " + apothem + "u " + 
+    System.out.println("the area of the hexagon which side is equal to " 
+            + side + "u" + ", and, its apothem is equal to " + apothem + "u " + 
             " is equal to --> " + area + "u2 ");
     
     
--- assignments/Unit1/HW02/AreaHexagonShort/src/areahexagonshort/AreaHexagonShort.java
+++ assignments/Unit1/HW02/AreaHexagonShort/src/areahexagonshort/AreaHexagonShort.java
@@ -15,8 +15,8 @@ public class AreaHexagonShort {
     apothem= (short) (side/1.15);
     area= (short) ((3*side)*apothem);
     
-    System.out.println("the area of the hexagon which side is equal to " + side + 
-            "u" + ", and, its apothem is equal to " + apothem + "u " + 
+    System.out.println("the area of the hexagon which side is equal to " 
+            + side + "u" + ", and, its apothem is equal to " + apothem + "u " + 
             " is equal to --> " + area + "u2 ");
     
     

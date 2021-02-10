1979f46106d88bcf7036bc715ddc13f904427c80
Leonela-Comina <leonelacomina@gmail.com>
2021-02-10 15:10:02 -0500

    WS12-Libraries

---
+++ workshops/unit2/WS12-Libraries/src/ws12/libraries/WS12Libraries.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws12.libraries;
+
+/**
+ *
+ * @author BIOSTAR
+ */
+public class WS12Libraries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

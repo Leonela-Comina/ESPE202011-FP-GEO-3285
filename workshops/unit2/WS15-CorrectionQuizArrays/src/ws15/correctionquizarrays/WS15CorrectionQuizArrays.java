84efc825b361f2a54d1dfb1c07c3a16dd157c44a
Leonela-Comina <leonelacomina@gmail.com>
2021-02-22 14:38:30 -0500

    WS15-CorrectionQuizArrays

--- workshops/unit2/WS15-CorrectionQuizArrays/src/ws15/correctionquizarrays/WS15CorrectionQuizArrays.java
+++ workshops/unit2/WS15-CorrectionQuizArrays/src/ws15/correctionquizarrays/WS15CorrectionQuizArrays.java
@@ -1,6 +1,8 @@
 
 package ws15.correctionquizarrays;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Leonela_Comina
@@ -8,7 +10,15 @@ package ws15.correctionquizarrays;
 public class WS15CorrectionQuizArrays {
  public static void main(String[] args) {
   
- 
+   int wild_animals = 0;
+  String[] animals;
+  int[] weight;
+  
+  Scanner scanner = new Scanner(System.in);
+   System.out.println("Enter the number of animals of which you want to see their weight ->");
+     wild_animals= scanner.nextInt();
+     
+     animals
     }
     
 }
